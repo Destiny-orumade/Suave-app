1d1e3b259f8b1f8299eaf2654ad7ee2f0addb5e1
Destiny-orumade <destinyorumade@gmail.com>
2025-04-28 22:41:36 +0100

    kinda done

--- suava-app/components/HeroSection.tsx
+++ suava-app/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 const HeroSection = () => {
     return (
-        <section className="relative flex flex-col items-center justify-center text-center bg-[url('/icons/hero.png')] bg-cover bg-center py-28 px-4 bg-[#012D3E]">
+        <section className="relative flex flex-col items-center justify-center text-center bg-[url('),url ('/icons/to.png)] bg-cover bg-center py-28 px-4 bg-[#012D3E] img-gradient-to-b from-[#012D3E] to-[#002D4F]">
             <h1 className=" text-white text-4xl md:text-6xl font-bold mb-6">About us </h1>
             <p className="text-gray-300 text-base md:text-lg max-w-2xl">
             Connecting fashion designers and brands with African fashion artists
